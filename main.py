05b4cfa2ed4b35d62c95de0b22176e3e1ffad1b8
sanchezvaleroe <sanchezvaleroe@educ.college-montreal.qc.ca>
2024-02-07 16:15:42 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+P = int(input())
+N = int(input())
+R = int(input())
+jour = 0
+
+for i in range():
+    if R > P:
+        print(jour)
+        break
+
+    elif R < P:
+        jour = jour + (N ** 2)
+
+    else:
+        break
